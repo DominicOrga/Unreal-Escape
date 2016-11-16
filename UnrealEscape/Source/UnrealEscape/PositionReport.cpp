00fc275f190ab99ac8878550d870fdbd472d401a
DominicOrga <dominicorga@gmail.com>
2016-11-17 05:47:00 +0800

    Report position object

--- UnrealEscape/Source/UnrealEscape/PositionReport.cpp
+++ UnrealEscape/Source/UnrealEscape/PositionReport.cpp
@@ -5,8 +5,7 @@
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
-{
+UPositionReport::UPositionReport() {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
@@ -20,7 +19,11 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectLocation = GetOwner()->GetActorLocation().ToString();
 
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectLocation);
 	// ...
 	
 }

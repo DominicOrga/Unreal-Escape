965a66003db6624f27b02fc2ee647059981748ca
DominicOrga <dominicorga@gmail.com>
2016-11-16 14:19:48 +0800

    First commit

---
+++ UnrealEscape/Source/UnrealEscape/UnrealEscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UnrealEscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALESCAPE_API AUnrealEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
